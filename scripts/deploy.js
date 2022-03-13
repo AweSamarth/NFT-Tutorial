d87ef3742724ba88d4fe1ca664380e4a6edcb259
Samarth Saxena <SamarthSaxena1672003@gmail.com>
2022-03-13 16:59:56 +0530

    ok

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+ const {ethers} = require("hardhat");
+ async function main(){
+     const contract= await ethers.getContractFactory("NFTee");
+     const deployedContract=await contract.deploy();
+     await deployedContract.deployed();
+     console.log("NFT contract deployed to:", deployedContract.address);
+ }
+
+ main()
+ .then(()=>process.exit(0))
+ .catch((error)=> {
+     console.error(error);
+     process.exit(1)
+ });
